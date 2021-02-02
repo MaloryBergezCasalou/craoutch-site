e6536861eb6943c9df8ad3d371ac3215bdac1c50
malorybergezcasalou <malory.bergezcasalou@ynov.com>
2021-02-02 16:13:11 +0100

    init

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/../public'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+let port = process.env.PORT || 3003;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`)
+});

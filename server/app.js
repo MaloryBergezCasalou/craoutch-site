eb3b0daaba6a27516598d3e069fe519f3caf79ae
Biozard01 <arthur.laforest33@gmail.com>
2021-02-04 15:55:43 +0100

    mongodb in

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 let app = express();
+const {
+    MongoClient
+} = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
 app.use(express.static(__dirname + '/../public'));
 app.get('/', (req, res) => {

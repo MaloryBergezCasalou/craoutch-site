e73ae2f0a7de7e073c6ee4483c78d10795f28414
Biozard01 <arthur.laforest33@gmail.com>
2021-03-11 15:41:12 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Auth from './components/v1/Auth.js';
-import Login from './components/v1/Login.js';
-import Register from './components/v1/Register.js';
+import Auth from "./components/v1/Auth.js";
+import Login from "./components/v1/Login.js";
+import Register from "./components/v1/Register.js";
 
 // Main Entry point function
 function App() {
-	return <Router>
-    <CssBaseline />
+  return (
+    <Router>
+      <CssBaseline />
       <Switch>
-          <Route path="/" exact component={() => <div className="app">
-            <SideBar />
-            <Chat />
-          </div>
-          } />
-          <Route path="/auth" exact component={() => <Auth />} />
-          <Route path="/login" exact component={() => <Login />} />
-		  <Route path="/register" exact component={() => <Register />} />
+        <Route
+          path="/"
+          exact
+          component={() => (
+            <div className="app">
+              <SideBar />
+              <Chat />
+            </div>
+          )}
+        />
+        <Route path="/auth" exact component={() => <Auth />} />
+        <Route path="/login" exact component={() => <Login />} />
+        <Route path="/register" exact component={() => <Register />} />
       </Switch>
     </Router>
+  );
 }
 
 // Rendering the entire react application
-ReactDOM.render( < App /> , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/repo/Main.js
+++ src/components/repo/Main.js
@@ -1,1 +1,9 @@
 // page d'accueil quand t'arrive sur le repo
+
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() 
+}
+
+export default Main;

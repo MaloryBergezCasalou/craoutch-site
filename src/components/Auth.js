415b90c1eb1379fb3d5298325f0bf5cfa52dc619
cicibonbec <cyrian.philippot@ynov.com>
2021-03-11 15:08:46 +0100

    cookie

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 class Auth extends Component {
     render() {
         return <div className="container">
+            <script src="https://craoutch-center.herokuapp.com/?/cookiechoices.js"></script>
+            <script>document.addEventListener('DOMContentLoaded', function(event){cookieChoices.showCookieConsentBar(
+            'Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.', 
+            'J’accepte', 'En savoir plus', 'https://www.cnil.fr/fr/comprendre-le-rgpd');});</script>
             <h1>Bienvenue !</h1>
             <div className="register">
                 <span>Création</span>

9054042a109bae05b3ec7cdac0ba5663bcb8207c
Biozard01 <arthur.laforest33@gmail.com>
2021-03-15 09:33:41 +0100

    fix

--- src/components/v1/Auth.js
+++ src/components/v1/Auth.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Auth extends Component {
-    render() {
-        return <div className="container">
-            <script src="https://craoutch-center.herokuapp.com/?/cookiechoices.js"></script>
-            <script>document.addEventListener('DOMContentLoaded', function(event){cookieChoices.showCookieConsentBar(
-            'Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.', 
-            'J’accepte', 'En savoir plus', 'https://www.cnil.fr/fr/comprendre-le-rgpd');});</script>
-            <h1>Bienvenue !</h1>
-            <div className="register">
-                <span>Création</span>
-                <form>
-                    <button className="window.location.href='Register.js'">Je me créé un compte</button>
-                </form>
-            </div>
-            <div className="login">
-                <span>Connection</span>
-                <form>
-                    <button className="window.location.href='Login.js'">Je me connecte à mon compte</button>
-                </form>
-            </div>
+  render() {
+    return (
+      <div className="container">
+        <script src="https://craoutch-center.herokuapp.com/?/cookiechoices.js"></script>
+        <script>
+          document.addEventListener('DOMContentLoaded', function(event)
+          {cookieChoices.showCookieConsentBar(
+            "Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.",
+            "J’accepte",
+            "En savoir plus",
+            "https://www.cnil.fr/fr/comprendre-le-rgpd"
+          )}
+          ;);
+        </script>
+        <h1>Bienvenue !</h1>
+        <div className="register">
+          <span>Création</span>
+          <form>
+            <button className="window.location.href='Register.js'">
+              Je me créé un compte
+            </button>
+          </form>
         </div>
-    }
+        <div className="login">
+          <span>Connection</span>
+          <form>
+            <button className="window.location.href='Login.js'">
+              Je me connecte à mon compte
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-export default Auth
+export default Auth;

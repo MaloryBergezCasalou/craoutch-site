64a380e5d21a5122b0adfd0d9a3a8c4f0bf565cc
mathieu.darribau@ynov.com <mathieu.darribau@ynov.com>
2021-03-29 15:46:07 +0200

    modif

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "./components/v1/Auth.js";
 import Login from "./components/v1/Login.js";
 import Register from "./components/v1/Register.js";
-import AddFile from "./components/repo/AddFile.js";
-import AuthR from "./components/repo/AuthR.js";
-import CreateRepo from "./components/repo/CreateRepo.js";
-import Main from "./components/repo/Main.js";
-import UpdateRepo from "./components/repo/UpdateRepo.js";
-
 
 // Main Entry point function
 function App() {
